a2b1e5c588956a30ca3fbf152fa01a9fa02037c4
Deyvid Rannyere <deyran@gmail.com>
2024-08-20 18:53:05 -0300

    Update

--- Part 3 - Navigation/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 3 - Navigation/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -12,6 +12,25 @@ public partial class MonkeysViewModel : BaseViewModel
         this.monkeyService = monkeyService;
     }
 
+
+    [RelayCommand]
+    async Task GoToDetailsAsync(Monkey monkey)
+    {
+        if (monkey is null)
+            return;
+
+        /*
+        1. Navigate to DetailsPage
+        2. To pass values to DetailsPage you can do as shown in the code below
+            await Shell.Current.GoToAsync($"{nameof(DetailsPage)}?id={monkey.Name}");
+
+        https://youtu.be/DuNLR_NJv8U?t=8987
+        */
+        await Shell.Current.GoToAsync($"{nameof(DetailsPage)}");
+
+    }
+
+
     [RelayCommand]
     async Task GetMonkeysAsync()
     {
@@ -23,10 +42,10 @@ public partial class MonkeysViewModel : BaseViewModel
             IsBusy = true;
             var monkeys = await monkeyService.GetMonkeys();
 
-            if(Monkeys.Count != 0)
+            if (Monkeys.Count != 0)
                 Monkeys.Clear();
-                
-            foreach(var monkey in monkeys)
+
+            foreach (var monkey in monkeys)
                 Monkeys.Add(monkey);
 
         }
